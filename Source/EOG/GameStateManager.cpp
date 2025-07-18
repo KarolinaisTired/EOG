b3ef69d41fb83fc1ab0dbe05e7a77569003205f8
Avalon Brathwaite <virlinthejammer@gmail.com>
2025-07-18 12:24:58 +0100

    Added assets, game state manager now works with a HasBunny bool ticked on or off.
    Added in a test box too.

--- Source/EOG/GameStateManager.cpp
+++ Source/EOG/GameStateManager.cpp
@@ -2,25 +2,114 @@
 
 
 #include "GameStateManager.h"
-
+#include "Engine/LevelStreamingDynamic.h"
 
 // Sets default values
 AGameStateManager::AGameStateManager()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// PrimaryActorTick.bCanEverTick = true;
 }
 
-// Called when the game starts or when spawned
 void AGameStateManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	CurrentState = EGameState::HouseOne;
+	FString EnumName = UEnum::GetValueAsString(CurrentState);
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, EnumName);
+
+	OnGameStateChanged.AddUniqueDynamic(this, &AGameStateManager::HandleGameStateChange);
+	FOnPlayerInHouse.AddUniqueDynamic(this, &AGameStateManager::OnPlayerInHouse);
 }
 
-// Called every frame
-void AGameStateManager::Tick(float DeltaTime)
+void AGameStateManager::HandleGameStateChange(EGameState CurState, bool CorrectBunnyChosen)
 {
-	Super::Tick(DeltaTime);
+	int32 PreviousHouseIndex = static_cast<int32>(CurrentState);
+	StreamedHouseLevel = nullptr; 
+	if (HouseLevelInstances.IsValidIndex(PreviousHouseIndex))
+	{
+		TSoftObjectPtr<UWorld> SoftLevelToUnload = HouseLevelInstances[PreviousHouseIndex];
+		UWorld* HouseToUnload = SoftLevelToUnload.LoadSynchronous();
+		if (HouseToUnload)
+		{
+			for (ULevelStreaming* Streamed : GetWorld()->GetStreamingLevels())
+			{
+				if (ULevelStreamingDynamic* Dyn = Cast<ULevelStreamingDynamic>(Streamed))
+				{
+					if (Dyn->GetWorldAsset() == HouseToUnload)
+					{
+						StreamedHouseLevel = Dyn; // Cache to unload 
+					}
+				}
+			}
+		}
+	}
+
+	int32 NewStateIndex = static_cast<int32>(CurrentState); 
+	if (CorrectBunnyChosen)
+	{
+		if (NewStateIndex < MaxStates)
+		{
+			NewStateIndex = static_cast<int32>(CurrentState) + 1; // Move onto the next house
+			CurrentState = static_cast<EGameState>(NewStateIndex);
+			FString EnumName = UEnum::GetValueAsString(CurrentState); 
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, EnumName);
+
+		}
+		else // Player has completed the final house 
+		{
+			// TODO Trigger maybe BP instance to show boxes burning and bunny's head coming off, 
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Omg you won queen"));
+		}
+		LoadHouse(NewStateIndex);
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Player did not have bunny"));
+		LoadHouse(NewStateIndex); 
+	}
 }
 
+void AGameStateManager::OnPlayerInHouse() // Unload previous level once player has entered a new house
+{
+	// TODO Unload previous instance
+	if (StreamedHouseLevel != nullptr)
+	{
+		StreamedHouseLevel->SetShouldBeLoaded(false);
+		StreamedHouseLevel->SetShouldBeVisible(false);
+	}
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Entered new house"));
+	// TODO Here we pull the associated house with the game state and load it
+}
+
+void AGameStateManager::LoadHouse(int StateIndex)
+{
+	if (HouseLevelInstances.IsValidIndex(StateIndex))
+	{
+		TSoftObjectPtr<UWorld> SoftLevelToLoad = HouseLevelInstances[StateIndex];
+		UWorld* HouseToLoad = SoftLevelToLoad.LoadSynchronous();
+		bool bLevelLoaded = false;
+
+		// TODO here we need to plug in the location of the previous level plus the same dimensions. 
+		FVector PlayerLocation = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation();
+		if (HouseToLoad)
+		{
+			ULevelStreamingDynamic* LoadedHouse = ULevelStreamingDynamic::LoadLevelInstanceBySoftObjectPtr(
+				GetWorld(), HouseToLoad, PlayerLocation, FRotator::ZeroRotator, bLevelLoaded);
+		}
+	}
+}
+
+void AGameStateManager::BroadcastStateChange(EGameState NewState, bool BunnyChosen)
+{
+	OnGameStateChanged.Broadcast(NewState, BunnyChosen);
+}
+
+void AGameStateManager::BroadcastPlayerInHouse()
+{
+	FOnPlayerInHouse.Broadcast(); 
+}
+
+EGameState AGameStateManager::GetCurrentState() const
+{
+	return CurrentState;
+}
--- Source/EOG/GameStateManager.h
+++ Source/EOG/GameStateManager.h
@@ -1,10 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
+#include "Engine/LevelStreamingDynamic.h"
 #include "GameFramework/Actor.h"
+#include "LevelInstance/LevelInstanceActor.h"
 #include "GameStateManager.generated.h"
+
+UENUM(BlueprintType)
+enum class EGameState : uint8
+{
+	HouseOne      UMETA(DisplayName = "House One"),
+	HouseTwo       UMETA(DisplayName = "House Two"),
+	HouseThree       UMETA(DisplayName = "HouseThree"),
+	HouseFour       UMETA(DisplayName = "House Four"),
+	HouseFive       UMETA(DisplayName = "House Five"),
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnGameStateChanged, EGameState, NewState, bool, CorrectBunnyChose);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPlayerInHouse);
 
 UCLASS(BlueprintType, Blueprintable)
 class EOG_API AGameStateManager : public AActor
@@ -13,10 +27,27 @@ class EOG_API AGameStateManager : public AActor
 
 public:
 	AGameStateManager();
+	UFUNCTION() void HandleGameStateChange(EGameState OldState, bool CorrectBunnyChosen);
+	UFUNCTION() void OnPlayerInHouse();
+	UFUNCTION() void LoadHouse(int StateIndex); 
+	
+	UFUNCTION(BlueprintCallable) void BroadcastStateChange(EGameState CurStateToBroadcast, bool BunnyChosen);
+	UFUNCTION(BlueprintCallable) void BroadcastPlayerInHouse();
+	UFUNCTION(BlueprintCallable) EGameState GetCurrentState() const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite) TArray<TSoftObjectPtr<UWorld>> HouseLevelInstances;
+	UPROPERTY(BlueprintReadWrite) FOnGameStateChanged OnGameStateChanged;
+	UPROPERTY(BlueprintReadWrite) FOnPlayerInHouse FOnPlayerInHouse;
+
+	UPROPERTY() EGameState CurrentState;
 
 protected:
 	virtual void BeginPlay() override;
 
-public:
-	virtual void Tick(float DeltaTime) override;
+private:
+	int MaxStates = 4;
+	UPROPERTY() ULevelStreamingDynamic* StreamedHouseLevel; 
 };
+
+// TODO if we choose wrong bunny, broadcast delegate with boolean of true or false, the BP of this will listen to that, if true, we get the current index of gamestate,
+// then set the CurrentState + 1 to whatever that is, then match that index to the ones in the level instance array, we then load that level instance in front of the current instance.
